08f2b8f1d5496b7773fc4eb42be6395049ca494e
chenbo <chenbotome@gmail.com>
2025-05-19 20:39:37 +0800

    fix

--- GQA-BACKEND/boot/migrate.go
+++ GQA-BACKEND/boot/migrate.go
@@ -3,13 +3,12 @@ package boot
 import (
 	"fmt"
 	"github.com/Junvary/gin-quasar-admin/GQA-BACKEND/global"
-	"github.com/Junvary/gin-quasar-admin/GQA-BACKEND/model"
 	"gorm.io/gorm"
 	"os"
 )
 
 func Migrate(db *gorm.DB) {
-	err := db.AutoMigrate(model.Models...)
+	err := db.AutoMigrate()
 	if err != nil {
 		fmt.Println("Database migration failed!", err.Error())
 		global.GqaSLogger.Error("Database migration failed!", "err", err.Error())
